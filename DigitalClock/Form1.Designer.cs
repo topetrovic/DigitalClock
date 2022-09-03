bb3d975d8569e6585ebc02438e4e86b6a92e9f01
Tomislav Petrović <topetrov@vsite.hr>
2022-09-03 20:41:22 +0200

    Some simple design for beginning

--- DigitalClock/Form1.Designer.cs
+++ DigitalClock/Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace DigitalClock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Wide Latin", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(122, 154);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(593, 83);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "20:00:00";
+           
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Wide Latin", 20F);
+            this.dateLabel.Location = new System.Drawing.Point(223, 335);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(387, 42);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "20.20.2020.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label dateLabel;
     }
 }
 
